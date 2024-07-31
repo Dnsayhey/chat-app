66a7deb7bd918ef8f53a27862fcbe1caeae340f1
dnsayhey <yanlei921@live.com>
2024-07-31 09:41:56 +0800

    MessageList component and add support for markdown

--- components/home/Main/index.tsx
+++ components/home/Main/index.tsx
@@ -1,13 +1,15 @@
 import Menu from './Menu'
 import Welcome from './Welcome'
 import ChatInput from './ChatInput'
+import MessageList from './MessageList'
 
 export default function Main() {
   return (
     <div className="flex-1 relative">
       <main className="flex-1 w-full h-full bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100 overflow-y-auto">
         <Menu />
-        <Welcome />
+        {/* <Welcome /> */}
+        <MessageList />
         <ChatInput />
       </main>
     </div>
