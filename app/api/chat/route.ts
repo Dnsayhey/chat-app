d7cf3e2c656cf2b51b8d2fc50bd7e632a4f28281
dnsayhey <yanlei921@live.com>
2024-08-01 02:11:02 +0800

    ...

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,8 +2,28 @@ import { sleep } from '@/common/utils'
 import { MessageRequestBody } from '@/types/chat'
 import { NextRequest, NextResponse } from 'next/server'
 
+import OpenAI from 'openai'
+
+const client = new OpenAI({
+  apiKey: 'test', // This is the default and can be omitted
+  baseURL: 'https://api.deepseek.com',
+})
+
 export async function POST(request: NextRequest) {
   const { messages } = (await request.json()) as MessageRequestBody
+  // test
+  const _stream = await client.chat.completions.create({
+    model: 'deepseek-chat',
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: 'Hello!' },
+    ],
+    stream: true,
+  })
+  for await (const chunk of _stream) {
+    process.stdout.write(chunk.choices[0]?.delta?.content || '')
+  }
+  // test
   const encoder = new TextEncoder()
   const stream = new ReadableStream({
     async start(controller) {
